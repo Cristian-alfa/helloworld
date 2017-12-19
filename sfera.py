43dc2d6680b6557e82f0763fba259649baea76af
Cristian-alfa <34458827+Cristian-alfa@users.noreply.github.com>
2017-12-20 00:35:14 +0100

    Add files via upload

---
+++ sfera.py
@@ -0,0 +1,10 @@
+#sfera.py
+
+
+import math
+r = input("iserire raggio")
+
+s = 4. * math.pi * r * r
+v = 4. / 3. * math.pi * r * r * r
+
+print "la superficie della sfera di raggio ", r, "e' ", s, "mentre il volume e' ", v
